f729995c2b55b61162d9ea7d300cd7d37803a994
etsehiywot <etsehiywotdb@gmail.com>
2025-03-03 09:01:10 -0800

    Initial commit

---
+++ blog/schemas.py
@@ -0,0 +1,40 @@
+from pydantic import BaseModel
+from typing import List
+from typing import Optional
+
+class BlogBase(BaseModel):
+    title: str
+    body: str
+   
+class Blog(BlogBase):
+    class Config:
+        from_attributes = True
+
+    
+class User(BaseModel):
+    name: str
+    email: str
+    password: str
+
+class ShowUser(BaseModel):
+    name: str
+    email: str
+    blogs: List[Blog] = []
+    class Config:
+        from_attributes = True
+
+class showBlog(BaseModel):
+    title: str
+    body: str
+    creator: ShowUser
+
+class Login(BaseModel):
+    username: str
+    password: str
+
+class Token(BaseModel):
+    access_token: str
+    token_type: str
+
+class TokenData(BaseModel):
+    email: Optional[str] = None
